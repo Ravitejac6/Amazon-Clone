7182060fb8cbd9178247964b4dda2deb77839146
Raviteja Penugonda <raviteja@eazydiner.com>
2021-05-13 15:35:07 +0530

    feat: React typescript app created
    - firebase config file added

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDCveLXmWVBG95EooQThtKWrG_nX0rsh_A",
+  authDomain: "clone-987a4.firebaseapp.com",
+  projectId: "clone-987a4",
+  storageBucket: "clone-987a4.appspot.com",
+  messagingSenderId: "643497027143",
+  appId: "1:643497027143:web:e19f269b275519bffd7362",
+  measurementId: "G-1E7LREV3P9",
+};
